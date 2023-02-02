0bd300d20a05886e7d886206a7c8a82e3fb1937b
Sema Yıldız <83815433+semayldz@users.noreply.github.com>
2023-02-02 23:10:48 +0300

    Update Soru1.py

--- Soru1.py
+++ Soru1.py
@@ -2,21 +2,23 @@
 # mesaj tipi oluşturun ve bunu bir topic üzerinden yayınlayın. 
 
 import rospy
-from geometry_msgs.msg import Twist
+from ros_essentials_cpp.msg import MyTwistMessage
 
 def turtle_vel():
 
-    pub = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size=10)
-    rospy.init_node('turtle_vel', anonymous=True)
-    linear_vel = 10
-    angular_vel = 30
+    pub = rospy.Publisher('my_twist_topic', MyTwistMessage, queue_size=10)
+    rospy.init_node('my_twist_publisher_node', anonymous=True)
+    
+    linear_speed = 10
+    angular_speed_degree = 30
+    angular_speed = math.radians(abs(angular_speed_degree))
 
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
-        vel_msg = Twist()
-        vel_msg.linear.x = linear_vel
-        vel_msg.angular.z = angular_vel
-        rospy.loginfo("Linear velocity: %f, Angular velocity: %f", linear_vel, angular_vel)
+        vel_msg = MyTwistMessage()
+        vel_msg.linear.x = linear_speed
+        vel_msg.angular.z = angular_speed
+        rospy.loginfo("Linear velocity: %f, Angular velocity: %f", linear_speed, angular_speed)
         pub.publish(vel_msg)
         rate.sleep()
 
