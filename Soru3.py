d06d4a00b272ad2202df0a667714fabc77e0f82b
Sema Yıldız <83815433+semayldz@users.noreply.github.com>
2023-02-02 23:00:41 +0300

    Update Soru3.py

--- Soru3.py
+++ Soru3.py
@@ -1,22 +1,58 @@
 # Turtlesim in pozisyonunu kontrol eden bir node yazınız
 
+#!/usr/bin/env python
+
 import rospy
+from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
+import math
+import time
+from std_srvs.srv import Empty
 
-def turtle_pose():
+def poseCallback(pose_message):
+    global x
+    global y, yaw
+    x = pose_message.x
+    y = pose_message.y
+    yaw = pose_message.theta
+  
+def go_to_goal(velocity_publisher, x_goal, y_goal):
+    global x
+    global y, yaw
+    velocity_message = Twist()
 
-    rospy.init_node('turtle_pose', anonymous=True)
-    
-    pose_msg = Pose()
-    pose_msg.x = float(input("x değerini giriniz: "))
-    pose_msg.y = float(input("y değerini giriniz: "))
-    pose_msg.theta = float(input("açı değerini giriniz: "))
-    
-    pose_publisher = rospy.Publisher('/turtle1/pose', Pose, queue_size=10)
-    pose_publisher.publish(pose_msg)
+    while (True):
+        K_linear = 0.5
+        distance = abs(math.sqrt(((x_goal-x) ** 2) + ((y_goal-y) ** 2)))
+        
+        linear_speed = distance * K_linear
 
-    if __name__ == '__main__':
-        try:
-            turtle_pose()
-        except rospy.ROSInterruptException:
-            pass
+        K_angular = 4.0
+        desired_angle_goal = math.atan2(y_goal-y, x_goal-x)
+        angular_speed = (desired_angle_goal-yaw) * K_angular
+
+        velocity_message.linear.x = linear_speed
+        velocity_message.angular.z = angular_speed
+
+        velocity_publisher.publish(velocity_message)
+        print ('x=', x, ', y=',y, ', distance to goal: ', distance)
+
+        if (distance <0.01):
+            break
+        
+       
+if __name__ == '__main__':
+    try:
+        rospy.init_node('turtlesim_motion_pose', anonymous=True)
+
+        cmd_vel_topic='/turtle1/cmd_vel'
+        velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
+        
+        position_topic = "/turtle1/pose"
+        pose_subscriber = rospy.Subscriber(position_topic, Pose, poseCallback) 
+        time.sleep(2)
+
+        go_to_goal(velocity_publisher, 1.0, 1.0)
+        
+    except rospy.ROSInterruptException:
+        rospy.loginfo("node terminated.")
